7add3ac333734557b3c23a515ed62eaa8d852491
Scot Dalton <scotdalton@gmail.com>
2013-05-03 16:19:08 -0400

    Update images http dir.

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ project_type = :stand_alone
 
 # Publishing paths
 http_path = "/"
-http_images_path = "/images"
+http_images_path = "/libtechnyu/images"
 http_fonts_path = "/fonts"
 css_dir = "public/stylesheets"
 
