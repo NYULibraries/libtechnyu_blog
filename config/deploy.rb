37a80079800f532ebef95c0bbeafbcf402bcdd5c
B Alter <barnaby.alter@gmail.com>
2014-03-05 15:04:37 -0500

    Changed ruby version in cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :app_title, "libtechnyu_blog"
 set :application, "#{app_title}_repos"
 
 # RVM  vars
-set :rvm_ruby_string, "1.9.3-p125"
+set :rvm_ruby_string, "1.9.3-p448"
 set :rvm_type, :user
 
 # Bundle vars
